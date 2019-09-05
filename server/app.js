3948a22c5c8b72b5b1ad49165b2187b55e27010c
hong焕佳 <hhuanjia@163.com>
2019-09-05 18:29:01 +0800

    create my blog

---
+++ server/app.js
@@ -0,0 +1,27 @@
+const Koa = require('koa'),
+      app = new Koa(),
+      koa = require('koa-router')(),
+      json = require('koa-json'),
+      logger = require('koa-logger'); //引入依赖
+
+      app.use(require('koa-bodyparser')());
+      app.use(json());
+      app.use(logger());
+
+      app.use(function* (next){
+        let start = new Date;
+        yield next;
+        let ms = new Date - start;
+        console.log('$s %s - %s', this.method, this.url, ms) // 显示执行时间
+      });
+
+      app.on('error', function(err, ctx){
+        console.log('server error', err);
+      });
+
+      app.listen(8889, ()=> {
+        console.log('Koa is listening in 8889');
+      })
+
+module.exports = app;
+
---
+++ src/main.js
@@ -0,0 +1,39 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import VueRouter from 'vue-router'
+
+Vue.use(ElementUI)
+Vue.use(VueRouter)
+
+import Login from './components/Login.vue'
+import TodoList from './components/TodoList.vue'
+
+const router = new VueRouter({
+  mode: 'history',
+  base: __dirname,
+  routes: [
+    {
+      path: '/',
+      component: Login
+    },
+    {
+      path: '/todolist',
+      component: TodoList
+    },
+    {
+      path: '*',
+      redirect: '/'
+    }
+  ]
+})
+
+
+/* eslint-disable no-new */
+const app = new Vue({
+  router: router,
+  render: h => h(App)
+}).$mount('#app')
